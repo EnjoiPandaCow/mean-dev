7045db27314820af3a2a716721a79ab08affcd7e
CJ O'Sullivan <cjosullivan@hotmail.co.uk>
2017-09-30 13:30:46 +0100

    Connected Node to Mongo

--- l6/api/controllers/hotels.controllers.js
+++ l6/api/controllers/hotels.controllers.js
@@ -1,8 +1,11 @@
+var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
-    console.log("GET the hotels");
-    console.log(req.query);
+
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
 
     var offset = 0;
     var count = 5;
@@ -15,26 +18,65 @@ module.exports.hotelsGetAll = function(req, res) {
         count = parseInt(req.query.count, 10);
     }
 
-    var returnData = hotelData.slice(offset,offset+count);
-
-    res
-        .status(200)
-        .json(returnData);
+    collection
+        .find()
+        .skip(offset)
+        .limit(count)
+        .toArray(function(err, docs){
+            console.log("Found hotels", docs);
+            res
+                .status(200)
+                .json(docs);
+        });
 };
 
 module.exports.hotelsGetOne = function(req, res) {
+
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId];
     console.log("GET hotelId", hotelId);
-    res
-        .status(200)
-        .json(thisHotel);
+
+    collection
+        .findOne({
+            _id : ObjectId(hotelId)
+        }, function(err, doc){
+            res
+                .status(200)
+                .json(doc);
+        });
 };
 
 module.exports.hotelAddOne = function(req, res) {
+
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    var newHotel;
+
     console.log("POST new hotel");
+
+    if(req.body && req.body.name && req.body.stars) {
+        newHotel = req.body;
+        newHotel.stars = parseInt(req.body.stars, 10);
+        collection.insertOne(newHotel, function(err, response){
+            console.log(response.ops);
+            res
+                .status(201)
+                .json(response.ops);
+        });
+    } else {
+        console.log("Data missing from body");
+        res
+            .status(400)
+            .json({message : "Required data missing from body"});
+
+    }
+
+    /*
     console.log(req.body);
     res
         .status(200)
         .json(req.body);
+        */
 };
